7aba8a70f21d1a16a94115f80f2a0b6585d5de8d
King the perse <141738897+SubhiChiku@users.noreply.github.com>
2024-07-16 10:05:45 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,11 +3,11 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_ID = int(getenv("API_ID"))
-API_HASH = getenv("API_HASH")
+API_ID = int(getenv("API_ID","21998505"))
+API_HASH = getenv("API_HASH","2ceae7fd0a32dcdb44561c7a3edebb53")
 
-BOT_TOKEN = getenv("BOT_TOKEN")
+BOT_TOKEN = getenv("BOT_TOKEN","7342588621:AAEwWQXb2v_5Topnrv-TmRd-x4q7a4ffUaw")
 #OWNER_ID = int(getenv("6664582540"))
-OWNER_ID = int(getenv("OWNER_ID", "6664582540"))
-MONGO_DB_URI = getenv("MONGO_DB_URI")
-MUST_JOIN = getenv("MUST_JOIN", None)
+OWNER_ID = int(getenv("OWNER_ID", "7181106700"))
+MONGO_DB_URI = getenv("MONGO_DB_URI","mongodb+srv://ahiravana999:ahiravana999@cluster0925.1torgfj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0925")
+MUST_JOIN = getenv("MUST_JOIN", "-1002242477930")
