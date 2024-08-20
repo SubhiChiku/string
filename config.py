9b3e884e740712a8808f8e9f6546c3c333e1d3cd
King the perse <141738897+SubhiChiku@users.noreply.github.com>
2024-08-20 11:22:01 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ load_dotenv()
 API_ID = int(getenv("API_ID","21998505"))
 API_HASH = getenv("API_HASH","2ceae7fd0a32dcdb44561c7a3edebb53")
 
-BOT_TOKEN = getenv("BOT_TOKEN","7342588621:AAEwWQXb2v_5Topnrv-TmRd-x4q7a4ffUaw")
+BOT_TOKEN = getenv("BOT_TOKEN","7242102837:AAGAv-USsh3v570_2RahxYsAIDcw1FPwUgg")
 #OWNER_ID = int(getenv("6664582540"))
 OWNER_ID = int(getenv("OWNER_ID", "7181106700"))
 MONGO_DB_URI = getenv("MONGO_DB_URI","mongodb+srv://ahiravana999:ahiravana999@cluster0925.1torgfj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0925")
